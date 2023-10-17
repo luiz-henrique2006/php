d2c86ebca2e46c311f9242c5ac082c9f23e2ba91
Luiz Henrique Viana Rocha <145489343+luiz-henrique2006@users.noreply.github.com>
2023-10-17 16:27:43 -0300

    aula 01 php

---
+++ aula01/exp04.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$txt = "Hello world";
+$x = 5;
+$y = 10.5;
+echo "15.5 " .$txt. "<br>";
+echo "oia ai luiz " .$x + $y. "<br>"; 
+?>
+</body>
+</html>
---
+++ aula01/exp05.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$curso = "WEB Médio - Aluno 10!";
+$disciplina = "PSW1";
+$turma = 2022;
+echo "[CURSO] : $curso<br>";
+echo "[DISCIPLINA] : $disciplina<br>";
+echo "[TURMA] : $turma<br>";
+echo "aiiii ", " zé da manga ", "sem pera hihihi ";
+?>
+    
+</body>
+</html>
