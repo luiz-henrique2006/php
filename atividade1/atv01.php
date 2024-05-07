7f54327b7aae6ae307c792b5c21cd4e67175f69d
Luiz Henrique Viana Rocha <145489343+luiz-henrique2006@users.noreply.github.com>
2024-05-07 13:59:54 -0300

    Add files via upload

---
+++ atividade1/atv01.php
@@ -0,0 +1,45 @@
+<?php
+
+$aluno["nome"] = "valdete viana";
+$aluno["endereco"] = "Rua 15";
+$aluno["cidade"] = "guanambi";
+$aluno["estado"] = "bahia";
+$aluno["idade"] = "50";
+$aluno["cpf"] = "781.689.393-6";
+
+$aluno1["nome"] = "carlos alberto";
+$aluno1["endereco"] = "rua barao do Rio Branco";
+$aluno1["cidade"] = "candiba";
+$aluno1["estado"] = "bahia";
+$aluno1["idade"] = "26";
+$aluno1["cpf"] = "780.070.907-9";
+
+$aluno2["nome"] = "erika";
+$aluno2["endereco"] = "rua sao paulo plínio";
+$aluno2["cidade"] = "vitoria da conquista";
+$aluno2["estado"] = "bahia";
+$aluno2["idade"] = "23";
+$aluno2["cpf"] = "919.000.890-9";
+
+$professor["nome"] = "ziraldo";
+$professor["endereco"] = "rua joqo pessoa";
+$professor["cidade"] = "joao pessoa";
+$professor["estado"] = "pernambuco";
+$professor["idade"] = "39";
+$professor["materia"] = "artes cenicas";
+
+$professor1["nome"] = "fabio";
+$professor1["endereco"] = "rua guanambi beija flor";
+$professor1["cidade"] = "londres";
+$professor1["estado"] = "naoseilandia";
+$professor1["idade"] = "18";
+$professor1["materia"] = "psw";
+
+$professor1["nome"] = "queila";
+$professor1["endereco"] = "rua pindai";
+$professor1["cidade"] = "ceraima";
+$professor1["estado"] = "guanambi";
+$professor1["idade"] = "59";
+$professor1["materia"] = "matematica";
+
+?>
