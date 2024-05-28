5a2c21867d217e08eb35bf47dd04927ffcecbf17
Luiz Henrique Viana Rocha <145489343+luiz-henrique2006@users.noreply.github.com>
2024-05-28 15:17:28 -0300

    Add files via upload

---
+++ aula-28-05/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+function nome_funcao($par_1 = 0, $par_2 = "vazio") {
+    echo "codigo de função <br>";
+    return "Dado de Rertono: ".$par_1."/".$par_2;
+}
+$retono = nome_funcao();
+echo $retorno. "<br><br>";
+$retorno = nome_funcao(12, "Bill Gates"); 
+echo $retorno."<br><br>;
+
+?>
+    
+</body>
+</html>
